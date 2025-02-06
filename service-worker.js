914f32a4110303d70e66dbcbdcbe587ed9971977
Beppaccio <beppe.allegretta@gmail.com>
2025-02-06 13:26:47 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'rata-finanziamento-v1';
+const urlsToCache = ['/', '/index.html', '/style.css', '/script.js'];
+
+// Installazione del Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch delle risorse dalla cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
